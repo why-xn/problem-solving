5d92966ff7701d92b3c7f7bf4d75d680836be592
Shihab Hasan <shihabhasan.iut@gmail.com>
2022-07-28 08:48:08 +0600

    Create 3sum.go
    https://leetcode.com/problems/3sum/submissions/

---
+++ neetcode/3sum.go
@@ -0,0 +1,38 @@
+func threeSum(nums []int) [][]int {
+    
+    var hset map[string]*byte = map[string]*byte{}
+    var res [][]int = [][]int{}
+    var i int = 0
+    var j int = len(nums) - 1
+    var diff int = 0
+    var hash string = ""
+    
+    sort.Ints(nums)
+    
+    for x, n := range nums {
+        i = x + 1;
+        j = len(nums) - 1
+        for i < j {
+            diff = 0 - (n + nums[i] + nums[j])
+            if diff == 0 {
+                hash = fmt.Sprintf("%d-%d-%d", n, nums[i], nums[j])
+                if _, ok := hset[hash]; ok {
+                    i++
+                    j--
+                    continue
+                }
+                hset[hash] = nil
+                res = append(res, []int{n, nums[i], nums[j]})
+                i++
+                j--
+            } else if diff > 0 {
+                i++
+            } else if diff < 0 {
+                j--
+            }
+        }
+    }
+    
+    return res
+    
+}
