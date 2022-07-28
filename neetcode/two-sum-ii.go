28911f6a6be68e5dce71d21f4a5ae74daadc5d71
Shihab Hasan <shihabhasan.iut@gmail.com>
2022-07-28 08:47:27 +0600

    Create two-sum-ii.go
    https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/submissions/

---
+++ neetcode/two-sum-ii.go
@@ -0,0 +1,18 @@
+func twoSum(numbers []int, target int) []int {
+    
+    var i int = 0
+    var j int = len(numbers) - 1
+    var diff int = 0
+    
+    for i < j {
+        diff = target - (numbers[i] + numbers[j])
+        if diff == 0 {
+            return []int{i+1, j+1}
+        } else if diff > 0 {
+            i++
+        } else if diff < 0 {
+            j--
+        }
+    }
+    return []int{}
+}
