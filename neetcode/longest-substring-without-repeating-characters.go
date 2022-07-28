fa8236d42d8bd94e7fc493b883606887b1178983
Shihab Hasan <shihabhasan.iut@gmail.com>
2022-07-28 19:12:12 +0600

    comments added

--- neetcode/longest-substring-without-repeating-characters.go
+++ neetcode/longest-substring-without-repeating-characters.go
@@ -11,10 +11,14 @@ func lengthOfLongestSubstring(s string) int {
     var max int = 0
     
     for r < len(s) {
+        // if character exists in hash, 
         if _, ok := hash[s[r]]; ok {
+            // as adding new character will create duplicacy in the hash, this is the max sequence
             if max < len(hash) {
                 max = len(hash)
             }
+            
+            // pop all the characters from hash until the matched character is removed from hash to avoid duplicacy
             for {
                 delete(hash, s[l])
                 if s[l] == s[r] {
@@ -26,10 +30,12 @@ func lengthOfLongestSubstring(s string) int {
             hash[s[r]] = nil
         }
         
+        // add character to hash
         hash[s[r]] = nil
         r++
     }
     
+    // final check
     if max < len(hash) {
         max = len(hash)
     }
