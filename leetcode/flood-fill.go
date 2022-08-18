0156f8c4cc49f7520a230757796fce96b02178aa
Shihab Hasan <shihabhasan.iut@gmail.com>
2022-08-18 16:24:42 +0600

    Update flood-fill.go

--- leetcode/flood-fill.go
+++ leetcode/flood-fill.go
@@ -5,7 +5,6 @@
 type FloodFill struct {
 	image      [][]int
 	queue      [][]int
-	hashSet    map[string]struct{}
 	startColor int
 }
 
@@ -14,7 +13,6 @@ func floodFill(image [][]int, sr int, sc int, color int) [][]int {
 	var ff FloodFill = FloodFill{
 		image:   image,
 		queue:   [][]int{},
-		hashSet: map[string]struct{}{},
 	}
 
 	if image[sr][sc] == color {
@@ -35,9 +33,8 @@ func doBfs(ff *FloodFill, color int) {
 		pos := ff.queue[0]
 		ff.queue = ff.queue[1:]
 
-		// Changing color and adding the pixel to hash set to save it as done
+		// Changing color
 		ff.image[pos[0]][pos[1]] = color
-		ff.hashSet[fmt.Sprintf("%d-%d", pos[0], pos[1])] = struct{}{}
 
 		// adding 4 directional pixels to the bfs queue
 		addToQueue(ff, []int{pos[0], pos[1] + 1}, color)
@@ -50,10 +47,8 @@ func doBfs(ff *FloodFill, color int) {
 }
 
 func addToQueue(ff *FloodFill, pos []int, color int) {
-	// checking if the pixel is already colored or eligible to be colored
-	if _, ok := ff.hashSet[fmt.Sprintf("%d-%d", pos[0], pos[1])]; !ok {
-		if pos[0] >= 0 && pos[1] >= 0 && pos[0] < len(ff.image) && pos[1] < len(ff.image[0]) && (ff.image[pos[0]][pos[1]] == ff.startColor) {
-			ff.queue = append(ff.queue, pos)
-		}
+	// checking if the pixel is eligible to be colored
+	if pos[0] >= 0 && pos[1] >= 0 && pos[0] < len(ff.image) && pos[1] < len(ff.image[0]) && ff.image[pos[0]][pos[1]] == ff.startColor {
+		ff.queue = append(ff.queue, pos)
 	}
 }
