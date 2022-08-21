f29d90e106151d522df3a35ae744b5a971c08270
Shihab Hasan <shihabhasan.iut@gmail.com>
2022-08-21 10:04:38 +0600

    Update number-of-islands.go

--- leetcode/number-of-islands.go
+++ leetcode/number-of-islands.go
@@ -14,11 +14,11 @@ func numIslands(grid [][]byte) int {
 		totalIslands: 0,
 	}
   
-  // Iterating over the grid to find a land
+        // Iterating over the grid to find a land
 	for i := 0; i < len(grid); i++ {
 		for j := 0; j < len(grid[0]); j++ {
 			if grid[i][j] == byte('1') {
-        // If an unmarked land if found then starting it as an island and marking all the lands of this island through DFS.
+                                // If an unmarked land if found then starting it as an island and marking all the lands of this island through DFS.
 				isf.totalIslands++
 				dfs(&isf, []int{i, j})
 			} else {
@@ -32,7 +32,7 @@ func numIslands(grid [][]byte) int {
 
 func dfs(isf *IslandFinder, pos []int) {
 	if isValidPos(isf, pos) {
-    // Marking the land as visited
+                // Marking the land as visited
 		isf.grid[pos[0]][pos[1]] = byte('2')
 
 		dfs(isf, []int{pos[0], pos[1] + 1})
@@ -43,7 +43,7 @@ func dfs(isf *IslandFinder, pos []int) {
 }
 
 func isValidPos(isf *IslandFinder, pos []int) bool {
-  // Only adding an unmarked land postion to DFS
+        // Only adding an unmarked land postion to DFS
 	if pos[0] >= 0 && pos[0] < len(isf.grid) && pos[1] >= 0 && pos[1] < len(isf.grid[0]) && isf.grid[pos[0]][pos[1]] == byte('1') {
 		return true
 	}
